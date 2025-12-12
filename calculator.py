d1b38e1b3296032da62fc1a7105ba8a1d7de5c8a
SahebCodeHub <sahebmandal172@gmail.com>
2025-12-12 19:54:21 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,33 @@
+
+def calculator():
+    num1 = float(input("Enter the first number : "))
+    num2 = float(input("Enter the second number: "))
+
+    print("\nChoose an operation:")
+    print("1. Addition (+)")
+    print("2. Subtraction (-)")
+    print("3. Multiplication (*)")
+    print("4. Division (/)")
+    
+    choice = input("Enter your choice (1/2/3/4): ")
+    if choice == '1':
+        result = num1 + num2
+        print("\nResult:", result)
+
+    elif choice == '2':
+        result = num1 - num2
+        print("\nResult:", result)
+
+    elif choice == '3':
+        result = num1 * num2
+        print("\nResult:", result)
+
+    elif choice == '4':
+        if num2 != 0:
+            result = num1 / num2
+            print("\nResult:", result)
+        else:
+            print("\nError: Cannot divide by zero!")
+    else:
+        print("\nInvalid choice! Please select 1–4 only.")
+calculator()
